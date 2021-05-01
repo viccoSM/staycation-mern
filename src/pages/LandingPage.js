b4b719f7366b07fe14e2895ee583c22ef701bb20
Vicco Semuel Manopo <63226256+viccoSM@users.noreply.github.com>
2021-05-01 11:37:04 +0800

    adding react-reveal

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,6 +5,8 @@ import MostPicked from "../parts/MostPicked";
 import Categories from "../parts/Categories";
 
 import landingPage from "../json/landingPage.json";
+import Testimony from "../parts/Testimony";
+import Footer from "../parts/Footer";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -22,6 +24,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
